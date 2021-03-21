e6ffaafdf61d6929f0fe25413920206f8300263f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-21 19:26:17 +0100

    twitch OAuth and sessions

--- src/app.js
+++ src/app.js
@@ -1,40 +1,105 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const session = require("express-session")
+const passport = require("passport")
+const twitchStrategy = require("passport-twitch-new").Strategy
+const findOrCreate = require('mongoose-findorcreate')
+
+const path = require("path")
+
+require('dotenv').config( {
+  path: path.join(__dirname, '.env')
+} );
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:2717/fruitsDB", {useNewUrlParser: true}, { useUnifiedTopology: true })
-
-const fruitSchema = new mongoose.Schema ({
-	name: String,
-	rating: Number,
-	review: String
-})
-
-const Fruit = mongoose.model("Fruit", fruitSchema);
-
-const fruit = new Fruit ({
-	name: "Apple",
-	rating: 7,
-	review: "solid"
-})
-
-fruit.save()
-
-
-
 app.set("view engine", "ejs")
-
 app.use(express.static("public"));
 
+// using sessions
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+}))
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session())
+
+// connect to database in docker
+mongoose.connect("mongodb://localhost:2717/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ })
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+	twitchId: String,
+	secret: String,
+});
+
+
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+// using Twitch OAuth
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+passport.use(new twitchStrategy({
+  clientID: process.env.TWITCH_CLIENT_ID,
+  clientSecret: process.env.TWITCH_CLIENT_SECRET,
+  callbackURL: "http://localhost:3001/auth/twitch/callback/",
+  scope: "user_read"
+},
+function(accessToken, refreshToken, profile, done) {
+  // Suppose we are using mongo..
+  console.log(profile)
+  User.findOrCreate({ twitchId: profile.id }, function (err, user) {
+    return done(err, user)
+  })
+}
+))
+
+
+// rendering pages
+app.get(
+	"/auth/twitch",
+	passport.authenticate("twitch")
+);
+
+app.get("/auth/twitch/callback", passport.authenticate("twitch", { failureRedirect: "/" }), function(req, res) {
+  // authenticated
+  res.redirect("/posts");
+});
+
 app.get("/", (req, res) => {
-	res.render("pages/home")
+	res.render("home")
 })
 
-app.get("/posts", (req, res) => {
-	res.render("pages/posts")
+app.get("/posts", function (req, res) {
+	if(req.isAuthenticated()){
+    res.render("posts");
+  } else {
+    res.redirect("/")
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login")
 })
 
 app.listen(3001, () => {
